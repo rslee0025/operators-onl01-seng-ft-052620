c2523a5af60833a9dafe9d064c646793fbe71ad6
Rachel S. Lee <rslee0025@gmail.com>
2020-05-27 21:01:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,31 +1,6 @@
 def unsafe?(speed)
-if speed == unsafe
-  return "true"
-else
-  return "false"
-end
-
-if speed > 60
-  return "true"
-else
-  return "false"
-end  
-
-if speed < 40
-  return "true"
-else
-  return "false"
-end
-
-if speed > 40 && speed < 60
-  return "false"
-else
-  return "true"
-end  
 
 end
-  
-  
 
 
 
