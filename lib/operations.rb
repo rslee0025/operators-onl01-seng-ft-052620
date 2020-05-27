220dec999b8431314557c8f7f1eabefb6f3af088
Rachel S. Lee <rslee0025@gmail.com>
2020-05-27 20:49:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,18 @@
 def unsafe?(speed)
-speed == unsafe  
+if speed == unsafe
+  return ""
   
   
-  
-speed > 60 ? "true" : "false"
-speed < 40 ? "true" : "false"
-speed <= 60 && speed >= 40 ? "false" : "true"
   
 
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? "true" : "false"
+speed < 40 ? "true" : "false"
+speed <= 60 && speed >= 40 ? "false" : "true"
+
 end
 	
 
